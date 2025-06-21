336dbd87a6e00f16c99493c1a5d670a78371bcdb
TsukiSama9292 <a0985821880@gmail.com>
2025-06-21 23:17:17 +0800

    [update] Steam - Let uv install in "/usr/local/bin" - Add gnome-keyring tree

--- src/ubuntu/install/tools/install_tools_deluxe.sh
+++ src/ubuntu/install/tools/install_tools_deluxe.sh
@@ -4,3 +4,5 @@ set -ex
 apt-get update
 apt-get install -y vlc git tmux
 
+# Add install gnome-keyring tree (edit from: TsukiSama9292)
+apt-get install -y gnome-keyring tree
--- src/ubuntu/install/uv/install_uv.sh
+++ src/ubuntu/install/uv/install_uv.sh
@@ -2,5 +2,5 @@
 set -e
 set -x
 
-# Install uv(for python)
-RUN curl -LsSf https://astral.sh/uv/install.sh | sh
+# Install uv(edit from: TsukiSama9292)
+curl -LsSf https://astral.sh/uv/install.sh | env UV_INSTALL_DIR="/usr/local/bin" INSTALLER_NO_MODIFY_PATH=1 sh
